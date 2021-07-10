14923cc7f0eba046c52ef4cb505e8379dba9d293
MrEmgin <bakin.d@liceum1535.ru>
2021-07-10 23:08:03 +0300

    refactored servos interface, func update_servos and global vars and arrays added, compiled, NOT TESTED, TODO: check index finger!

--- testing_servos/testing_servos.ino
+++ testing_servos/testing_servos.ino
@@ -4,15 +4,22 @@
 #define MIN_PINKY 0 //мизинец (пин 10) закрыт
 #define MAX_RING_FINGER 120 //безымянный палец (пин 9) открыт
 #define MIN_RING_FINGER 0 //безымянный палец (пин 9) закрыт
-#define MAX_MIDDLE_FINGER 0 //средний палец (пин 6) закрыт
-#define MIN_MIDDLE_FINGER 120 //средний палец (пин 6) открыт
-#define MAX_INDEX_FINGER 0 //указательный палец (пин 5) закрыт
-#define MIN_INDEX_FINGER 120 //указательный палец (пин 5) открыт
+#define MAX_MIDDLE_FINGER 120 //средний палец (пин 6) закрыт
+#define MIN_MIDDLE_FINGER 0 //средний палец (пин 6) открыт
+#define MAX_INDEX_FINGER 40 //указательный палец (пин 5) закрыт
+#define MIN_INDEX_FINGER 70 //указательный палец (пин 5) открыт
 #define MAX_THUMB 120 //большой палец (пин 3) открыт
 #define MIN_THUMB 0 //большой палец (пин 3) закрыт
 
+int max_min_angles[] = {MAX_PINKY, MIN_PINKY,
+                        MAX_RING_FINGER, MIN_RING_FINGER,
+                        MAX_MIDDLE_FINGER, MIN_MIDDLE_FINGER,
+                        MAX_INDEX_FINGER, MIN_INDEX_FINGER,
+                        MAX_THUMB, MIN_THUMB};
+
 //задаем скорости движения пальцев
 //при необходимости их можно исправлять здесь
+#define SPEED 2
 #define SPEED_PINKY 2
 #define SPEED_RING_FINGER 2
 #define SPEED_MIDDLE_FINGER -2
@@ -26,6 +33,9 @@ Servo servo_middle_finger; //указательный палец (Сервопр
 Servo servo_index_finger; //средний палец (Сервопривод Tower Pro MG995, пин 5 на шилде)
 Servo servo_thumb; //безымянный и мизинец (Сервопривод Tower Pro MG995, пин 3 на шилде)
 
+Servo servos[] = {servo_pinky, servo_ring_finger, servo_middle_finger, servo_index_finger, servo_thumb};
+int angles[] = {MIN_PINKY, MIN_RING_FINGER, MIN_MIDDLE_FINGER, MIN_INDEX_FINGER, MIN_THUMB};
+
 int t1,t2,t3,t4,t5 = 0; //переменные для хранения угла поворота сервы
 int emg1, emg2 = 0; //переменные для хранения НЕобработанных значений ЭМГ-сигнала
 int amp1, amp2 = 0; //переменные для хранения обработанных значений ЭМГ-сигнала
@@ -34,7 +44,19 @@ int threshhold2 = 30;
 int min1, min2 = 255; //переменые для хранения минимальных значений обработанного сигнала
 int max1, max2 = 0; //переменые для хранения максимальных значений обработанного сигнала
 
-
+void update_servos(int loop_with_delay_num=1){
+  int current_angle, new_angle;
+  for(byte i=0; i<loop_with_delay_num; ++i){
+   for(byte j=0; i<sizeof(servos)/sizeof(int); ++i){
+    if (angles[j] < max_min_angles[j*2] && angles[j] > max_min_angles[j*2 + 1]){ // if servo can move any further
+      current_angle = servos[j].read();
+      if (current_angle < angles[j]) new_angle = current_angle + SPEED;
+      else new_angle = current_angle - SPEED;
+      servos[j].write(new_angle);
+    }
+   }
+  }
+}
 void clunch(){
   while (servo_pinky.read() > MIN_PINKY ||
  servo_ring_finger.read() > MIN_RING_FINGER || 
@@ -42,15 +64,15 @@ void clunch(){
  servo_index_finger.read() < MIN_INDEX_FINGER ||
  servo_thumb.read() > MIN_THUMB){
  //управление валом каждого сервопривода, знак + или - означает в какую сторону поворачивается вал
- t1 = servo_pinky.read() - SPEED_PINKY;
+ t1 = servo_pinky.read() - SPEED_PINKY * (int)(servo_pinky.read() > MIN_PINKY);
  servo_pinky.write(t1);
- t2 = servo_ring_finger.read() - SPEED_RING_FINGER;
+ t2 = servo_ring_finger.read() - SPEED_RING_FINGER * (int)(servo_ring_finger.read() > MIN_RING_FINGER);
  servo_ring_finger.write(t2);
- t3 = servo_middle_finger.read() - SPEED_MIDDLE_FINGER;
+ t3 = servo_middle_finger.read() + SPEED_MIDDLE_FINGER * (int)(servo_middle_finger.read() < MIN_MIDDLE_FINGER);
  servo_middle_finger.write(t3);
- t4 = servo_index_finger.read() + SPEED_INDEX_FINGER;
+ t4 = servo_index_finger.read() + SPEED_INDEX_FINGER * (int)(servo_index_finger.read() < MIN_INDEX_FINGER);
  servo_index_finger.write(t4);
- t5 = servo_thumb.read() - SPEED_THUMB;
+ t5 = servo_thumb.read() - SPEED_THUMB * (int)(servo_thumb.read() > MIN_THUMB);
  servo_thumb.write(t5);
  delay(15);
  }
@@ -63,15 +85,15 @@ void relax(){
  servo_index_finger.read() > MAX_INDEX_FINGER ||
  servo_thumb.read() < MAX_THUMB){
  //управление валом каждого сервопривода, знак + или - означает в какую сторону поворачивается вал
- t1 = servo_pinky.read() + SPEED_PINKY;
+ t1 = servo_pinky.read() + SPEED_PINKY * (int)(servo_thumb.read() > MIN_THUMB);
  servo_pinky.write(t1);
- t2 = servo_ring_finger.read() + SPEED_RING_FINGER;
+ t2 = servo_ring_finger.read() + SPEED_RING_FINGER * (int)(servo_ring_finger.read() < MAX_RING_FINGER);
  servo_ring_finger.write(t2);
- t3 = servo_middle_finger.read() + SPEED_MIDDLE_FINGER;
+ t3 = servo_middle_finger.read() - SPEED_MIDDLE_FINGER * (int)(servo_middle_finger.read() > MAX_MIDDLE_FINGER);
  servo_middle_finger.write(t3);
- t4 = servo_index_finger.read() - SPEED_INDEX_FINGER;
+ t4 = servo_index_finger.read() - SPEED_INDEX_FINGER * (int)(servo_index_finger.read() > MAX_INDEX_FINGER);
  servo_index_finger.write(t4);
- t5 = servo_thumb.read() + SPEED_THUMB;
+ t5 = servo_thumb.read() + SPEED_THUMB * (int)(servo_thumb.read() < MAX_THUMB);
  servo_thumb.write(t5);
  delay(15);
  }
@@ -90,10 +112,15 @@ void setup() {
 }
 
 void loop() {
- for (byte i=0; i < 3; ++i){
+  /*int delay_millis = 1000;
   clunch();
-  delay(1000);
-  relax(); 
- }
-
+  Serial.println("clunched");
+  delay(delay_millis);
+  relax();
+  Serial.println("relaxed");
+  delay(delay_millis);*/
+  update_servos();
+  delay(15);
+  for(byte i=0; i < 5; ++i) Serial.println(String(servos[i].read()));
+  Serial.println("");
 }
